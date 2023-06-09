56c71f5d1213750d39ac691bb9c2aebe47554723
Aman Ullah Khan <amanullahkhan81828@gmail.com>
2023-06-09 12:57:23 +0500

    commit

---
+++ js.js
@@ -0,0 +1,70 @@
+// var car = {
+//     name: "civic",
+//     model: 2020,
+//     isAutumatic: true,
+//     color: 'red'
+// }
+
+// console.log(car.name);
+
+
+
+// var civic = {
+
+//     model: 2020,
+//     isAutumatic: true,
+//     color: ['red', 'black', 'silver']
+// }
+
+// civic.fuelMode = 'Hybrid';
+// civic.isAutumatic = false;
+// console.log(civic);
+
+
+var user = {};
+user.name = prompt('what is your name?');
+user.rollNumber = prompt('what is your roll number?');
+user.age = prompt('what is your age?');
+user.hobbies = [];
+for(var i = 0; i < 2; i++){
+    var hobby = prompt('enter 1 hobby');
+    user.hobbies.push(hobby);
+    user.hobbies[i] = hobby;
+}
+user.age = 22;
+console.log(user);
+
+
+
+// var user = {
+//     pcConfig: {
+//         ram: {
+//             companyName: 'kingston',
+//             memory: '32GB'
+//         },
+//         processor: "i7",
+//         SSD: '1TB',
+//         graphicsCard: "RTX3018"
+//     },
+//     gadgets: ['Gaming laptop', "macbook", 'iPad'],
+//     name: 'Akram',
+//     age: 26,
+//     isMarried: false,
+// }
+
+// user.pcConfig.ram.companyName = 'Samsung';
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
